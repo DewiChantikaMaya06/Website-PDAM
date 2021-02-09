03cb40bcd9e2c5b8418a292d9468cbe3b4e74b05
Aditya Novan Firmansyah <55123299+AdityaNovanF@users.noreply.github.com>
2021-02-10 05:15:24 +0700

    info

--- app/Berita.php
+++ app/Berita.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Berita extends Model
 {
     protected $table = 'berita';
-    protected $fillable = ['judul', 'sumber', 'gambar_berita', 'tanggal', 'isi_berita'];
+    protected $fillable = ['judul', 'sumber', 'gambar_berita', 'isi_berita'];
 }
---
+++ database/migrations/2021_02_09_221122_create_info_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('unit');
+            $table->integer('pegawai');
+            $table->integer('pengunjung');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('info');
+    }
+}
