5449c06b6170df1f94d0efb9c232ef64c3d0841a
Aditya Novan Firmansyah <55123299+AdityaNovanF@users.noreply.github.com>
2021-02-10 13:19:10 +0700

    oke

--- resources/views/info/index.blade.php
+++ resources/views/info/index.blade.php
@@ -10,9 +10,9 @@
                     <div class="panel">
                         <div class="panel-heading">
                             <h4>{{ $title }}</h4>
-                            <div class="right">
+                            <!-- <div class="right">
                                 <a href="{{ url('info/add') }}" type="button" class="btn"><i class="lnr lnr-plus-circle"></i></a>
-                            </div>
+                            </div> -->
                         </div>
                         @if(session('sukses'))
                         <div class="alert alert-success alert-dismissible" role="alert">
@@ -35,31 +35,29 @@
                                 <table class='table myTable'>
                                     <thead>
                                         <tr>
-                                            <th>No.</th>
                                             <th>unit</th>
                                             <th>Pegawai</th>
                                             <th>Pengguna</th>
                                             <th>Edit</th>
-                                            <th>Hapus</th>
+                                            <!-- <th>Hapus</th> -->
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @foreach($data as $e=>$dt)
+                                        @foreach($data as $dt)
                                         <tr>
-                                            <td>{{$e+1}}</td>
                                             <td>{{$dt->unit}}</td>
                                             <td>{{$dt->pegawai}}</td>
                                             <td>{{$dt->pengguna}}</td>
                                             <td>
                                                 <a href='{{ url('info/'.$dt->id) }}' class="btn btn-warning btn-edit" id="edit"><i class="fa fa-pencil-square-o"></i></a>
                                             </td>
-                                            <td>
+                                            <!-- <td>
                                                 <form method='post' action='{{url('info/delete/'.$dt->id)}}'>
                                                     @csrf
                                                     <input type="hidden" name="_method" value="DELETE">
                                                     <button type="submit" class="btn btn-danger" onclick="return confirm('Yakin mau dihapus ?') "><i class="fa fa-trash-o"></i></button>
                                                 </form>
-                                            </td>
+                                            </td> -->
                                         </tr>
                                         @endforeach
                                     </tbody>
@@ -72,5 +70,4 @@
         </div>
     </div>
 </div>
-
 @endsection
